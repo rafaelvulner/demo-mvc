600778d2dc71fec1151101b1e5f77a745fa11e5b
Rafael Elias <rafael.elias@prax.net.br>
2019-04-27 20:15:59 -0300

    Created primary commit with spring boot and thymeleaf

---
+++ src/main/java/com/monteiro/curso/boot/demomvc/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.monteiro.curso.boot.demomvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/index")
+	public String index() {
+		
+		return "Teste";
+	}
+
+}
